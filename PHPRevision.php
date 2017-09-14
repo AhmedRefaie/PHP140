8e5e783bf1fbfb56d9f7002d49c70ef71ff6d7e5
Ahmed Refaie <ahmedomargreat@gmail.com>
2017-09-14 19:30:14 +0200

    Till video 20

---
+++ PHPRevision.php
@@ -0,0 +1,357 @@
+<?php
+
+echo 'Hello Programing'; /*to print stuff on screen and you can use print too*/
+
+
+phpinfo();/*info about your php*/
+
+
+
+
+echo '<strong>Na3san ya fa5ry.</strong>';/* for more strong text*/
+
+
+
+echo "<input type='text' name='name' value='Refaie'>";/*take care single couts inside big outside and viseversa*/
+
+
+
+<?php
+$ref = 'Ahmed Refaie';
+?>
+/*mixing php with html */
+<input type='text' name='name' value='<?php echo $ref; ?>'>
+
+
+
+
+echo $Ref = 'Refaie el Dso2y.';
+echo $number = 100;
+/*var and php awsome izi print */
+echo $Ref;
+echo $number;
+
+
+
+echo 'the date is <strong>'.$day.' '.$date.' '.$year.'</strong>';/* hard way to join text*/
+
+
+
+echo "the date is $day $date $year";/* lazy ezi way*/
+
+
+
+if(1){
+	echo 'TRUE.';	
+}else {
+	echo 'False.';
+}/*1 is automatic true 0 is automatic false*/
+
+
+
+$text ='Sometihng';
+
+if($text=='Sometihng'){
+	echo 'TRUE.';	
+}else {
+	echo 'False.';
+}/*text for sure also*/
+
+
+
+
+
+$number = 13;
+
+if($number==10){
+	echo'Equal to ten.';
+}else if ($number==11){
+	echo 'Equal to elevn.';
+}else{
+	echo'WTF is this number';
+}/*if else stamten to make more condition or allowance*/
+
+
+
+
+$number1 = 10;
+
+$number1 +-*/%= 2;
+
+echo $number1;/*Assignment Operations*/
+
+
+
+$text = 'Hello';
+
+$text .= ' Refaie.';
+/* more ass operators */
+echo $text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
